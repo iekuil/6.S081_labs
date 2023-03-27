9acec492809c970302a8f6a70ecaf1aaac99c51f
suibian <suibian.uvw@gmail.com>
2023-03-27 16:58:56 +0800

    Uthread finished

--- user/uthread.c
+++ user/uthread.c
@@ -10,10 +10,27 @@
 #define STACK_SIZE  8192
 #define MAX_THREAD  4
 
+struct regs {
+    uint64  ra;
+    uint64  sp;
+    uint64  s0;
+    uint64  s1;
+    uint64  s2;
+    uint64  s3;
+    uint64  s4;
+    uint64  s5;
+    uint64  s6;
+    uint64  s7;
+    uint64  s8;
+    uint64  s9;
+    uint64  s10;
+    uint64  s11;
+};
 
 struct thread {
   char       stack[STACK_SIZE]; /* the thread's stack */
   int        state;             /* FREE, RUNNING, RUNNABLE */
+  struct    regs    reg;
 };
 struct thread all_thread[MAX_THREAD];
 struct thread *current_thread;
@@ -62,6 +79,8 @@ thread_schedule(void)
      * Invoke thread_switch to switch from t to next_thread:
      * thread_switch(??, ??);
      */
+
+    thread_switch((uint64)&(t->reg),(uint64) &(current_thread->reg));
   } else
     next_thread = 0;
 }
@@ -76,6 +95,9 @@ thread_create(void (*func)())
   }
   t->state = RUNNABLE;
   // YOUR CODE HERE
+  (t->reg).sp = (uint64)(t->stack+8192);
+  (t->reg).ra = (uint64)func;
+
 }
 
 void 
